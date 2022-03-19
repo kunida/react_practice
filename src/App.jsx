57a18c62d137a554ad29be58a102b1752c8be194
kunida <67056950+kunida@users.noreply.github.com>
2022-03-19 20:29:20 +0900

    export のやり方ふたつめ

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+// import ColorfulMessage from "./components/ColorfulMessage";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
   console.log("スタート");
