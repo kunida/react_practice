51045ebd8989a67bd729ede14d15babf6a4250c0
kunida <67056950+kunida@users.noreply.github.com>
2022-03-19 19:18:01 +0900

    JSX記法

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react"; //react17でJSXのみ使用の時はimport必要なし
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    // 一つのタグで囲わないといけない<div>とかで
+    // <div>を使いたくない時は<React.Fragment>
+    //今はさらに短縮して<>でもいい
+    <>
+      <h1>Hello!!</h1>
+      <p>How are you?</p>
+    </>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
