cd942ac89464e58dde43cc3c35b416bfa21c0162
kunida <67056950+kunida@users.noreply.github.com>
2022-03-19 19:24:01 +0900

    コンポーネント

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react"; //react17でJSXのみ使用の時はimport必要なし
 import ReactDom from "react-dom";
+import App from "./App"; //コンポーネント
 
-const App = () => {
-  return (
-    // 一つのタグで囲わないといけない<div>とかで
-    // <div>を使いたくない時は<React.Fragment>
-    //今はさらに短縮して<>でもいい
-    <>
-      <h1>Hello!!</h1>
-      <p>How are you?</p>
-    </>
-  );
-};
+// const App = () => {
+//   return (
+//     // 一つのタグで囲わないといけない<div>とかで
+//     // <div>を使いたくない時は<React.Fragment>
+//     //今はさらに短縮して<>でもいい
+//     <>
+//       <h1>Hello!!</h1>
+//       <p>How are you?</p>
+//     </>
+//   );
+// };
 
 ReactDom.render(<App />, document.getElementById("root"));
