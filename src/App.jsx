c702fb7bec67b13656333a6cd20f374bb8b8d364
kunida <67056950+kunida@users.noreply.github.com>
2022-03-19 19:32:48 +0900

    イベント、CSS

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  return (
+    //{}はJSと認識される
+    //css　直接書くか変数にしていれるか
+    //イベント {}で中に関数を入れてそっちで処理
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
